a889f872aa630610ca8fbf8f55d28aa5a1690d5d
Daniel Don <dandons25@gmail.com>
2019-06-15 18:33:29 +0100

    update test file and app.js -add test to know if robot tals=ks strings -update app.js to have all the robot commands from the tutorial

--- test/appTest.js
+++ test/appTest.js
@@ -1,19 +1,24 @@
 // Unit Testing with Mocha and Chai
 
 const assert = require('chai').assert
+const App = require('../app')
 
-describe('Field test for killer Robot', () => {
+describe('robot tests are successful', () => {
 
     let num = 88;
     let string = 'ajnjkfhuihgekjfnvkfv'
+    let robot = App 
 
     it("check that num is a number", () => {
         assert.isNumber(num)
     });
-    
+
     it("check that string is a String", () => {
         assert.isString(string)
     });
+    it("check that Robot talks strings", () => {
+        assert.isString(robot.talk(), 'string')
+    })
 
 
 
